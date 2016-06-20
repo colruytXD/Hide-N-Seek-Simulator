d6816ea653ee4e428ed16c9cfcf56ce4a6c522e7
tom willaert <tom willaert>
2016-06-20 20:58:23 +0200

    !Very! basic networking implemented

---
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    private string gameVersion = "0.01";
+    public string roomName = "room";
+
+    public string playerPrefabName = "FPSController";
+    public Transform spawnPoint;
+
+	void OnEnable() 
+	{
+		SetInitialReferences();
+
+        PhotonNetwork.ConnectUsingSettings(gameVersion);
+	}
+
+	void OnDisable() 
+	{
+
+	}
+
+	void SetInitialReferences() 
+	{
+
+	}
+
+    void OnJoinedLobby()
+    {
+        RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 4 };
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(playerPrefabName, spawnPoint.position, spawnPoint.rotation, 0);
+    }
+}
---
+++ Assets/Scripts/Networking/NetworkPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkPlayer : MonoBehaviour {
+
+	void OnEnable() 
+	{
+		SetInitialReferences();
+	}
+
+	void OnDisable() 
+	{
+
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	void SetInitialReferences() 
+	{
+
+	}
+}
