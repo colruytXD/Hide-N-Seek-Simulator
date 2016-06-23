f88222b12fb11e24d2a9e3a8be0053e43e88cef3
tom willaert <tom willaert>
2016-06-23 14:42:20 +0200

    Played around with raycasts

--- Assets/Scripts/GameMech Scripts/GameMech_HitPlayer.cs
+++ Assets/Scripts/GameMech Scripts/GameMech_HitPlayer.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class GameMech_HitPlayer : MonoBehaviour {
 
     private GameMech_Master gameMechMasterScript;
+    private RaycastHit hit;
+    [SerializeField]
+    private float hitRange = 5;
 
 	void OnEnable() 
 	{
@@ -17,20 +20,24 @@ public class GameMech_HitPlayer : MonoBehaviour {
 	
 	void Update () 
 	{
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            DestroyThis();
-        }
+        CheckForPlayer();
 	}
 
 	void SetInitialReferences() 
 	{
-        gameMechMasterScript = GameObject.FindGameObjectWithTag("GameHandler").GetComponent<GameMech_Master>();
 	}
 
-    [PunRPC]
-    public void DestroyThis()
+    void CheckForPlayer()
     {
-        PhotonNetwork.Destroy(GetComponent<PhotonView>());
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("mouse0 pressed");
+            Debug.DrawRay(gameObject.transform.position, gameObject.transform.forward, Color.blue, 10f);
+            if(Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, hitRange, LayerMask.NameToLayer("Player")))
+            {
+                Debug.Log("hit " + hit.transform.name);
+                hit.transform.GetComponent<PhotonView>().RPC("GetHit", PhotonTargets.All);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/GameMech Scripts/GameMech_PlayerGotHit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMech_PlayerGotHit : MonoBehaviour {
+
+	void OnEnable() 
+	{
+		SetInitialReferences();
+	}
+
+	void OnDisable() 
+	{
+
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	void SetInitialReferences() 
+	{
+
+	}
+
+    [PunRPC]
+    public void GetHit()
+    {
+        Debug.Log("got hit");
+    }
+}
