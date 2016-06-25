2c3859e4d487fa05a32d61e4de5424fd5a00f1a0
tom willaert <tom willaert>
2016-06-25 21:52:44 +0200

    Added Loading screen

---
+++ Assets/Scripts/GameManager Scripts/GameManager_DisableLoadingScreen.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager_DisableLoadingScreen : Photon.MonoBehaviour {
+
+    [SerializeField]
+    private GameObject loadingCamera;
+
+    void OnEnable()
+    {
+        SetInitialReferences();
+    }
+
+    void Start()
+    {
+        if(photonView.isMine)
+        {
+            DisableLoadingCamera();
+        }
+    }
+
+    void SetInitialReferences()
+    {
+        loadingCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    void DisableLoadingCamera()
+    {
+        loadingCamera.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI Scripts/UI_LoadingScreen.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_LoadingScreen : MonoBehaviour {
+
+    private Text txtLoading;
+    [SerializeField]
+    private float waitTime = 0.1f;
+
+	void OnEnable() 
+	{
+		SetInitialReferences();
+        ChangeLoadingText();
+	}
+
+	void SetInitialReferences() 
+	{
+        txtLoading = GetComponent<Text>();
+	}
+
+    void ChangeLoadingText()
+    {
+        StartCoroutine(one());
+    }
+
+    IEnumerator one()
+    {
+        txtLoading.text = "Loading.";
+        yield return new WaitForSeconds(waitTime);
+        StartCoroutine(two());
+    }
+
+    IEnumerator two()
+    {
+        txtLoading.text = "Loading..";
+        yield return new WaitForSeconds(waitTime);
+        StartCoroutine(tree());
+    }
+
+    IEnumerator tree()
+    {
+        txtLoading.text = "Loading...";
+        yield return new WaitForSeconds(waitTime);
+        StartCoroutine(one());
+    }
+}
